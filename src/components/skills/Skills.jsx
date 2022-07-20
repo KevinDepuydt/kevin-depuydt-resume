454fbadcb011f7f413da98f30816c3fd75a3e85b
KevinDepuydt <kevin.dpdt@gmail.com>
2022-07-20 21:13:46 +0200

    french content

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
 import { Container } from 'components/common/containers.styled';
 import { SectionHeader } from 'components/common/headers.styled';
+import { List, ListItem } from 'components/common/lists.styled';
 
+
+const SKILLS_I18N_KEYS = [
+  "programming.main",
+  "programming.others",
+  "practices",
+  "management",
+  "tools",
+  "english",
+  "french"
+];
 
 export default function Skills() {
+  const { t } = useTranslation();
+  const items = t('skills.items', { returnObjects: true }) ?? [];
+  console.log(items);
   return (
     <Container>
-      <SectionHeader>Skills</SectionHeader>
+      <SectionHeader>{t('skills.title')}</SectionHeader>
+      <List>
+        {items.map((item, index) => (
+          <ListItem key={index}>{item}</ListItem>
+        ))}
+      </List>
     </Container>
   );
 }
