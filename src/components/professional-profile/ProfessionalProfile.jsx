5959336a3f494cf0679125ff39652a9ba33b244f
Kevin Depuydt <kevin.dpdt@gmail.com>
2024-02-08 15:58:39 +0100

    feat: dynamically compute years of activity in professional profile component

--- src/components/professional-profile/ProfessionalProfile.jsx
+++ src/components/professional-profile/ProfessionalProfile.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Container } from 'components/common/containers.styled';
 import { Text } from 'components/professional-profile/ProfessionalProfile.styled';
+import { getDeveloperActivityYears } from '../../helpers/dates';
 
+
+const DEVELOPER_ACTIVITY_YEARS = getDeveloperActivityYears();
 
 export default function ProfessionalProfile() {
   const { t } = useTranslation();
   return (
     <Container>
-      <Text>{t('professional.profile.introduction')}</Text>
+      <Text>{t('professional.profile.introduction', { years: DEVELOPER_ACTIVITY_YEARS })}</Text>
     </Container>
   );
 }
